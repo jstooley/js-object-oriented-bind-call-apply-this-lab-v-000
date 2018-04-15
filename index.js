ad1ad0b3942c2b95129ae563a52dd5ed11a5940e
Justin Tooley <jstooley823@gmail.com>
2018-04-15 21:04:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function justInvoke(fn){
 function setThisWithCall(fn, thisValue, args) {
   return fn.call(thisValue, args)
 }
-function setThisWithApply(){
-  
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args)
 }
